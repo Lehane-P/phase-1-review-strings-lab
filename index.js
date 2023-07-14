1b94121393e66673ef74be51476a77b56aa45a05
Lehane-P <Lehane-P>
2023-07-14 10:50:12 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Lehane";
+const welcomeMessage = "Welcome to Flatbook, ${Lehane}!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${LEHANE}!"
+const shortGreeting = "Welcome, ${L}!"
